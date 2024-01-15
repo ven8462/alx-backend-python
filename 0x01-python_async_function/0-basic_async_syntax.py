f67363d6805a051d2a090c75d76fe7925892ffac
lavender <lavenderanyango9@gmail.com>
2024-01-15 22:55:27 +0300

    async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
-"""using the  asyncio and random module """
+"""using the  asyncio and random module"""
 
 import asyncio
 import random
-from typing import Optional
 
 
-async def wait_random(max_delay: Optional[float] = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     an asynchronous coroutine that takes in an integer argument,
     delays it for some time and returns it
     """
-
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
